6986a501aaacbcd0ffad6948d8973dd898813eb0
Christine Xiang <yutingcxiang@gmail.com>
2018-06-20 06:46:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,9 +17,9 @@ function init() {
     const key = e.key;
     if (key === codes[index]) {
       index++;
-    if (index === codes.length) {
-      alert("congrats!");
-      index = 0;
+      if (index === codes.length) {
+        alert("congrats!");
+        index = 0;
      }
      } else {
     index = 0;
