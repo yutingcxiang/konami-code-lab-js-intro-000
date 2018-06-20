b9b8189025ffcd0b9146acb2be28cb423a3d0faa
Christine Xiang <yutingcxiang@gmail.com>
2018-06-20 06:44:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   let index = 0;
-  document.addEventListener('keydown', functio(e) {
+  document.addEventListener('keydown', function (e) {
     const key = e.key;
     if (key === codes[index]) {
       index++;
